4ac38f3e6d27989b7ba5f0dcf58571c16d891be5
Sagnik Ghosh <sagnikKGP123@gmail.com>
2026-07-18 17:08:33 +0530

    Time: 6 ms (29.00%) | Memory: 43.8 MB (15.20%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int count = 0;
+        for(int i = 0;i<citations.length;i++){
+            if(citations[i]>=(citations.length-i)) count++;
+        }
+        return count;
+    }
+}
